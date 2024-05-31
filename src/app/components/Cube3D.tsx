373c9389f2e8d4b8bb2c3c4e992c6d3e72d5156a
byhow <by.how@hotmail.com>
2024-05-31 01:10:31 -0700

    feat: use custom react props

--- src/app/components/Cube3D.tsx
+++ src/app/components/Cube3D.tsx
@@ -3,28 +3,31 @@ import { initializeGeometryCube } from "@/lib/moves";
 import { Canvas } from "@react-three/fiber";
 
 import { camera, scene, stickerMesh } from "@/lib/render";
-import { useState } from "react";
 import { Sticker } from "@/lib/types";
 
-export default function Cube3D() {
-  const geoCube = initializeGeometryCube();
-  const [stickers, setStickers] = useState<Sticker[]>(geoCube);
+type Cube3DProps = {
+  stickers: Sticker[];
+};
 
-  return (
-    <Canvas scene={scene(stickerMesh(geoCube))} camera={camera()}>
-      <mesh>
-        <ambientLight intensity={Math.PI / 2} />
-        <spotLight
-          position={[10, 10, 10]}
-          angle={0.15}
-          penumbra={1}
-          decay={0}
-          intensity={Math.PI}
-        />
-        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
-    </Canvas>
-  );
-}
+const Cube3D = ({ stickers }: Cube3DProps) => (
+  <Canvas
+    scene={scene(stickerMesh(stickers ?? initializeGeometryCube()))}
+    camera={camera()}
+  >
+    <mesh>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <boxGeometry />
+      <meshStandardMaterial />
+    </mesh>
+  </Canvas>
+);
+
+export default Cube3D;
